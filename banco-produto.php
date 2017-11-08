d2e6024080fb6293625e32043c9d5aafb8b546c2
Fernando Avellar <fernando.avellar@inatel.br>
2017-11-08 17:47:12 -0200

    Apos encerrar curso 2 de PHP da Alura

--- banco-produto.php
+++ banco-produto.php
@@ -1,6 +1,10 @@
 <?php
+require_once("conecta.php");
 
 function insereProduto($nome, $preco, $descricao, $categoria_id, $usado, $conexao) {
+    $nome = mysqli_real_escape_string($conexao, $nome);
+    $preco = mysqli_real_escape_string($conexao, $preco);
+    $descricao = mysqli_real_escape_string($conexao, $descricao);
     $query = "insert into produtos (nome, preco, descricao, categoria_id, usado) values ('{$nome}', {$preco}, '{$descricao}', {$categoria_id}, {$usado})";
     return mysqli_query($conexao, $query);
 }
--- produto-altera-formulario.php
+++ produto-altera-formulario.php
@@ -1,8 +1,7 @@
 <?php 
-include("cabecalho.php"); 
-include("conecta.php"); 
-include("banco-categoria.php"); 
-include("banco-produto.php"); 
+require_once("cabecalho.php"); 
+require_once("banco-categoria.php"); 
+require_once("banco-produto.php"); 
 
 $id = $_GET["id"];
 $produto = buscaProduto($id, $conexao);
@@ -14,35 +13,7 @@ $usado = $produto['usado'] ? "checked='checked'" : "";
 <form action="altera-produto.php" method="post">
     <table class="table">
     <input type="hidden" name="id" value="<?=$produto['id']?>">
-        <tr>
-            <td>Nome</td>
-            <td><input type="text" class="form-control" name="nome" value="<?=$produto['nome']?>" /></td>
-        </tr>
-        <tr>
-            <td>Preço</td>
-            <td><input type="number" class="form-control" name="preco" value="<?=$produto['preco']?>" /></td>
-        </tr>
-        <tr>
-            <td>Descrição</td>
-            <td><textarea name="descricao" class="form-control"><?=$produto['descricao']?></textarea></td>
-        </tr>
-        <tr>
-            <td></td>
-            <td><input type="checkbox" name="usado" value="true" <?=$usado?>> Usado</td>
-        </tr>
-        <tr>
-            <td>Categoria</td>
-            <td>
-                <select name="categoria_id" class="form-control">
-                    <?php foreach($categorias as $categoria) : 
-                        $essaEhACategoria = $produto['categoria_id'] == $categoria['id'];
-                        $selecao = $essaEhACategoria ? "selected='selected'" : "";
-                    ?>
-                    <option value="<?=$categoria['id']?>" <?=$selecao?>><?=$categoria['nome']?></option>
-                    <?php endforeach ?>
-                </select>
-            </td>
-        </tr>
+    <?php include("produto-formulario-base.php"); ?>  
         <tr>
             <td></td>
             <td><input type="submit" value="Alterar" class="btn btn-primary" /></td>
